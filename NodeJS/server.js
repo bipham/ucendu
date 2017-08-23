803c89bc7d471d21ebb237169ebcf6ba624db6b4
Bi Pham <phamtuananh.bkit@gmail.com>
2017-08-24 00:15:35 +0700

    fix port server

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -3,8 +3,8 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var redis = require('redis');
 
-server.listen(8890);
-console.log('Server running port: 8890 ...');
+server.listen(80);
+console.log('Server running port: 80 ...');
 current_sockets = {};
 io.on('connection', function (socket) {
     console.log("new client connected: " + socket.id);
