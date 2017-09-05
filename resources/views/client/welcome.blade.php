7c229052d685b6022e82d5d8610920eb93302ad7
Bi Pham <phamtuananh.bkit@gmail.com>
2017-09-05 14:10:57 +0700

    fix welcome admin

--- resources/views/client/welcome.blade.php
+++ resources/views/client/welcome.blade.php
@@ -7,7 +7,7 @@
  */
 //var_dump($highest_result[1]);
 ?>
-@extends('layout.masterNoMenu')
+@extends('layout.master')
 @section('meta-title')
     Welcome Ucendu
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@
 
 Route::pattern('nameDomain', '(www.ucendu.dev|ucendu.dev|www.ucendu.com|ucendu.com|www.ucendu.vn|ucendu.vn)');
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 // Authentication routes...
 Route::get('login', ['as'=>'getLogin', 'uses' => 'Auth\LoginController@getLogin']);
 Route::post('login',['as'=>'postLogin','uses'=>'Auth\LoginController@postLogin']);
@@ -42,6 +38,10 @@ Route::get('markAllNotificationAsRead',['as'=>'markAllNotificationAsRead','uses'
  *********************************************************/
 
 Route::group(['domain' => 'admin.{nameDomain}'], function () {
+    Route::get('/', function () {
+        return view('admin.welcome');
+    });
+
     Route::get('uploadReadingLesson',['as'=>'getUploadReadingLesson','uses'=>'Admin\ReadingLessonController@getUploadReadingLesson']);
     Route::post('uploadReadingLesson',['as'=>'postUploadReadingLesson','uses'=>'Admin\ReadingLessonController@postUploadReadingLesson']);
 
@@ -84,6 +84,10 @@ Route::group(['domain' => 'admin.{nameDomain}'], function () {
  *
  *********************************************************/
 Route::group(['domain'=>'{nameDomain}', 'middleware' => ['clientAuth']], function () {
+    Route::get('/', function () {
+        return view('client.welcome');
+    });
+
     Route::get('showComments/{question_id_custom}',['as'=>'showComments','uses'=>'Client\CommentQuestionController@getComments']);
     Route::get('showKeywords/{question_id_custom}',['as'=>'showKeywords','uses'=>'Client\CommentQuestionController@getKeywords']);
 
